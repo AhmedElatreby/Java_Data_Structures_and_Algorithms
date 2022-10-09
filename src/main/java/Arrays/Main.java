5394b3b6f1d150c1508356aab79a8806a13bae96
ahmedsport <ahmedsport@hotmail.com>
2022-10-09 21:32:56 +0100

    Array Traversal

--- src/main/java/Arrays/Main.java
+++ src/main/java/Arrays/Main.java
@@ -3,11 +3,13 @@ package Arrays;
 public class Main {
     public static void main(String[] args) {
         SingleDimensionArray sArray = new SingleDimensionArray(10);
-        sArray.insert(0,0);
-        sArray.insert(1,10);
-        sArray.insert(2,20);
-        sArray.insert(1,30);
-        sArray.insert(12,120);
+        sArray.insert(0, 0);
+        sArray.insert(1, 10);
+        sArray.insert(2, 20);
+        sArray.insert(1, 30);
+        sArray.insert(12, 120);
 
+        System.out.println("Array Traversal");
+        sArray.traversalArray();
     }
 }
--- src/main/java/Arrays/SingleDimensionArray.java
+++ src/main/java/Arrays/SingleDimensionArray.java
@@ -22,4 +22,17 @@ public class SingleDimensionArray {
             System.out.println("Invalid index to access array");
         }
     }
+
+    // Array Traversal
+
+    public void traversalArray() {
+        try{
+            for (int i = 0; i < array.length; i++) {
+                System.out.print(array[i] + " ");
+            }
+        } catch (Exception e) {
+            System.out.println("Array no longer exists!");
+        }
+
+    }
 }
