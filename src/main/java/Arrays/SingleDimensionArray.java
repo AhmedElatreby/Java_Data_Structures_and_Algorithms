6035f8057f3e5b216ad23377bffbd0c97510ec67
ahmedsport <ahmedsport@hotmail.com>
2022-10-09 21:11:12 +0100

    Single Dimension Array

---
+++ src/main/java/Arrays/SingleDimensionArray.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class SingleDimensionArray {
+    int array[] = null;
+
+    public SingleDimensionArray(int sizeOfArray) {
+        array = new int[sizeOfArray];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Integer.MIN_VALUE;
+        }
+    }
+
+    public void insert(int location, int valueToBeInserted) {
+        try {
+            if (array[location] == Integer.MIN_VALUE) {
+                array[location] = valueToBeInserted;
+                System.out.println("Successfully inserted");
+            } else {
+                System.out.println("This cell is already occupied");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index to access array");
+        }
+    }
+}
