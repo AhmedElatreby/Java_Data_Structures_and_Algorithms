63df46dabade0e1c95312141839d1ef5b101c06a
AhmedElatreby <ahmedsport@hotmail.com>
2022-10-23 19:39:32 +0100

    Single Linked List

---
+++ src/main/java/LinkedList/Single/Main.java
@@ -0,0 +1,9 @@
+package LinkedList.Single;
+
+public class Main {
+    public static void main(String[] args) {
+SingleLinkedList sLL = new SingleLinkedList();
+sLL.createSingleLinkedList(5);
+        System.out.println(sLL.head.value);
+    }
+}
