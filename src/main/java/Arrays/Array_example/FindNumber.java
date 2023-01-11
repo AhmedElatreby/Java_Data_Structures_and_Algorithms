772fd3a08bea4f2663855e82e0a0b1cc6ded9cb2
ahmedsport <ahmedsport@hotmail.com>
2023-01-11 13:54:15 +0000

    find number

--- src/main/java/Arrays/Array_example/FindNumber.java
+++ src/main/java/Arrays/Array_example/FindNumber.java
@@ -1,13 +1,20 @@
 package Arrays.Array_example;
 
 public class FindNumber {
-    public void findNumber(int[] number, int target) {
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] == target) {
-                System.out.println(target + " is found at the index of " + i + " in the array");
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        findNumber(arr, 100);
+
+    }
+
+    public static void findNumber(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (target == arr[i]) {
+                System.out.println(target + " found at index of " + i);
                 return;
             }
         }
-        System.out.println(target + " is not found!");
+        System.out.println(target + " not found");
     }
 }
+
--- src/main/java/Arrays/Array_example/MissingNumber.java
+++ src/main/java/Arrays/Array_example/MissingNumber.java
@@ -1,13 +1,15 @@
 package Arrays.Array_example;
 
 public class MissingNumber {
-    public void missingNumber(int[] array){
+
+
+    public void missingNumber(int[] array) {
         int sum1 = 0;
         int sum2 = 0;
         for (int i : array) {
             sum1 += i;
         }
-        sum2 = 100*(100 + 1)/2;
+        sum2 = 100 * (100 + 1) / 2;
         int result = sum2 - sum1;
         System.out.println("The missing number is: " + result);
 
